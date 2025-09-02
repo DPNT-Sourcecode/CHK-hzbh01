f27295ff9ae50cf1f5165d52cc48db5825e4abb1
sbx_user1051 <sbx_user1051@169.254.97.153>
2025-09-02 10:42:25 +0000

    Tue Sep 02 10:42:25 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,16 +1,24 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         int sum = 0;
+        List<Character> itemsHandled = new ArrayList<>();
         for(char c: skus.toCharArray()){
-            int count = skus.chars().filter(t -> t == c).count();
-            sum += getSingleItemPrice(c);
+            if(itemsHandled.contains(c)){
+                break;
+            }
+            long count = skus.chars().filter(t -> t == c).count();
+            sum += getSingleItemPrice(c, count);
+            itemsHandled.add(c);
         }
         return sum;
     }
 
-    private Integer getSingleItemPrice(char item, int count) {
+    private Integer getSingleItemPrice(char item, long count) {
         return switch (item) {
             case 'A' -> 50;
             case 'B' -> 30;
@@ -24,6 +32,7 @@ public class CheckoutSolution {
 
 // receive items as string ? multiple items ? how seperated ?
 // assuming skus is a lsit of
+
 
 
 
