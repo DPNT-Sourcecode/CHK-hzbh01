e7ab47df989d9cd7091c9acaa79f916fb523521f
sbx_user1051 <sbx_user1051@169.254.97.153>
2025-09-02 10:33:25 +0000

    Tue Sep 02 10:33:25 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,10 +1,26 @@
 package io.accelerate.solutions.CHK;
 
-import io.accelerate.runner.SolutionNotImplementedException;
-
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        
+        int sum = 0;
+        switch(skus) {
+            case "A" : sum += 50;
+            break;
+            case "B" : sum += 30;
+            break;
+            case "C" : sum += 20;
+            break;
+            case "D" : sum += 15;
+        }
+        return sum;
     }
+
+    private Integer getSingleItemPrice() 
 }
+
+
+// receive items as string ? multiple items ? how seperated ?
+// assuming skus is a lsit of
+
+
 
