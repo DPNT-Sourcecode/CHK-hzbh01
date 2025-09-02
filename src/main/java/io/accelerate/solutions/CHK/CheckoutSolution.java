8c79b11215b16305ac8a4dc1482f3325c76883f3
sbx_user1051 <sbx_user1051@169.254.97.153>
2025-09-02 10:39:25 +0000

    Tue Sep 02 10:39:25 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -4,12 +4,13 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         int sum = 0;
         for(char c: skus.toCharArray()){
+            int count = skus.chars().filter(t -> t == c).count();
             sum += getSingleItemPrice(c);
         }
         return sum;
     }
 
-    private Integer getSingleItemPrice(char item) {
+    private Integer getSingleItemPrice(char item, int count) {
         return switch (item) {
             case 'A' -> 50;
             case 'B' -> 30;
@@ -23,6 +24,7 @@ public class CheckoutSolution {
 
 // receive items as string ? multiple items ? how seperated ?
 // assuming skus is a lsit of
+
 
 
 
